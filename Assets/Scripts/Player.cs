e8d8f01783df48d14e419b873def88907525426b
KJeffree <k.jeffree@gmail.com>
2019-03-25 11:05:18 +0000

    player moves left and right with animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] Sprite[] walking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+        var newXPos = transform.position.x + deltaX/8;
+        transform.position = new Vector2(newXPos, transform.position.y);
+        if (deltaX > 0)
+        {
+            AnimatePlayerWalkingRight();
+        }
+        else if (deltaX < 0)
+        {
+            AnimatePlayerWalkingLeft();
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = walking[0];
+        }
+    }
+
+    private void AnimatePlayerWalkingRight()
+    {
+        Sprite playerSprite = GetComponent<SpriteRenderer>().sprite;
+        if (playerSprite != walking[1])
+        {
+            GetComponent<SpriteRenderer>().sprite = walking[1];
+        } 
+        else if (playerSprite == walking[1]) 
+        {
+            GetComponent<SpriteRenderer>().sprite = walking[2];
+        }
+    }
+     private void AnimatePlayerWalkingLeft()
+    {
+        Sprite playerSprite = GetComponent<SpriteRenderer>().sprite;
+        if (playerSprite != walking[3])
+        {
+            GetComponent<SpriteRenderer>().sprite = walking[3];
+        } 
+        else if (playerSprite == walking[3]) 
+        {
+            GetComponent<SpriteRenderer>().sprite = walking[4];
+        }
+    }
+}
