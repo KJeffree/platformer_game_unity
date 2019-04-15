d30f29ee5ef0f0e9ac2a026dea31678743784eee
KJeffree <k.jeffree@gmail.com>
2019-04-15 11:55:17 +0100

    fine tuned animations and refactored code for player. Also removed the steps in Scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,36 +28,48 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveVertical();
-        MoveHorizontal();
+        // MoveVertical();
+        // MoveHorizontal();
 
-        //var vertical = Input.GetAxis("Vertical");
-        //var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
 
-        //if (vertical == 0 && horizontal == 0)
-        //{
-        //    animator.SetInteger("Action", 0);
-        //}
-        //else if (vertical == 0 && horizontal > 0)
-        //{
-        //    animator.SetInteger("Action", 1);
-        //}
-        //else if (vertical == 0 && horizontal < 0)
-        //{
-        //    animator.SetInteger("Action", 2);
-        //}
-        //else if (vertical != 0 && horizontal > 0)
-        //{
-        //    animator.SetInteger("Action", 4);
-        //}
-        //else if (vertical != 0 && horizontal < 0)
-        //{
-        //    animator.SetInteger("Action", 5);
-        //}
-        //else if (canClimb & horizontal == 0)
-        //{
-        //    animator.SetInteger("Action", 3);
-        //}
+        if (vertical == 0 && horizontal == 0)
+        {
+           animator.SetInteger("Action", 0);
+        }
+        else if (vertical == 0 && horizontal > 0)
+        {
+            MoveHorizontal();
+            movingRight = true;
+           animator.SetInteger("Action", 1);
+        }
+        else if (vertical == 0 && horizontal < 0)
+        {
+            MoveHorizontal();
+            movingRight = false;
+            animator.SetInteger("Action", 2);
+        }
+        else if (vertical != 0 && movingRight && !canClimb)
+        {
+            MoveVertical();
+            MoveHorizontal();
+            movingRight = true;
+            animator.SetInteger("Action", 4);
+        }
+        else if (vertical != 0 && !movingRight && !canClimb)
+        {
+            MoveVertical();
+            MoveHorizontal();
+            movingRight = false;
+            animator.SetInteger("Action", 5);
+        }
+        else if (canClimb)
+        {
+            MoveVertical();
+            MoveHorizontal();
+            animator.SetInteger("Action", 3);
+        }
     }
 
     private void MoveHorizontal()
@@ -65,26 +77,6 @@ public class Player : MonoBehaviour
         var deltaX = Input.GetAxis("Horizontal") * speed;
         var movement = deltaX *= Time.deltaTime;
         transform.Translate(movement, 0, 0);
-        
-        if (deltaX > 0 && grounded)
-        {
-            animator.SetInteger("Action", 1);
-            movingRight = true;
-        }
-        else if (deltaX < 0 && grounded)
-        {
-            animator.SetInteger("Action", 2);
-            movingRight = false;
-        }
-        else if (movingRight && grounded)
-        {
-            animator.SetInteger("Action", 0);
-        }
-        else if (!movingRight && grounded)
-        {
-            animator.SetInteger("Action", 0);
-
-        }
     }
     private void MoveVertical()
     {
@@ -92,71 +84,19 @@ public class Player : MonoBehaviour
         {
             grounded = false;
             rigidBody.AddForce(new Vector3(0, 5, 0), ForceMode2D.Impulse);
-             AnimatePLayerJumping();
         }
         else if (canClimb)
         {
             var deltaY = Input.GetAxis("Vertical");
             var newYPos = transform.position.y + deltaY / 10;
             transform.position = new Vector2(transform.position.x, newYPos);
-            AnimatePlayerClimbing();
         }
     }
-
-    private void AnimatePlayerClimbing()
-    {
-        if (canClimb && !grounded)
-        {
-            animator.SetInteger("Action", 3);
-        }
-    }
-
-    // private void AnimatePlayerWalkingRight()
-    // {
-    //     Sprite playerSprite = GetComponent<SpriteRenderer>().sprite;
-    //     if (playerSprite != walking[0])
-    //     {
-    //         ChangeSprite(walking, 0);
-    //     } 
-    //     else if (playerSprite == walking[0]) 
-    //     {
-    //         ChangeSprite(walking, 1);
-    //     }
-    // }
-    //  private void AnimatePlayerWalkingLeft()
-    // {
-    //     Sprite playerSprite = GetComponent<SpriteRenderer>().sprite;
-    //     if (playerSprite != walking[2])
-    //     {
-    //         ChangeSprite(walking, 2);
-    //     } 
-    //     else if (playerSprite == walking[2]) 
-    //     {
-    //         ChangeSprite(walking, 3);
-    //     }
-    // }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         grounded = true;
     }
-
-     private void AnimatePLayerJumping()
-     {
-         if (movingRight && !grounded)
-         {
-            animator.SetInteger("Action", 4);
-         }
-        else if (!movingRight && !grounded)
-         {
-            animator.SetInteger("Action", 5);
-         }
-    }
-
-    // private void ChangeSprite(Sprite[] action, int index)
-    // {
-    //     GetComponent<SpriteRenderer>().sprite = action[index];
-    // }
 
     public void ClimbingLadder()
     {
