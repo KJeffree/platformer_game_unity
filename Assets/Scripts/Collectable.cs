77c0175b625b88aa09e12bd78a04902f74dd9057
KJeffree <k.jeffree@gmail.com>
2019-04-18 10:52:04 +0100

    gem can be collected, and Level has variable counting gems

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+
+    Level level;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CountCollectableGems();
     }
 
     // Update is called once per frame
@@ -19,5 +21,15 @@ public class Collectable : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Destroy(gameObject);
+        level.RemoveGem();
+    }
+
+    private void CountCollectableGems()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "CollectableGem")
+        {
+            level.CountGems();
+        }
     }
 }
