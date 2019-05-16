fc79d1573cb176fbd7e2a62229cfd0f268d0435a
KJeffree <k.jeffree@gmail.com>
2019-05-16 14:31:44 +0100

    can jump on spikes and lose health

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -19,6 +19,10 @@ public class Level : MonoBehaviour
 
     [SerializeField] Sprite[] availableHealth;
 
+    [SerializeField] int currentHeartIndex = 0;
+
+    [SerializeField] int healthReduced = 0;
+
     // public void CountGems()
     // {
     //     collectableGems++;
@@ -72,8 +76,41 @@ public class Level : MonoBehaviour
        }
     }
 
+    public void CheckCurrentHeart()
+    {
+        if (healthReduced <= 2)
+        {
+            currentHeartIndex = 0;
+        }
+        if (healthReduced > 2 && healthReduced <= 5)
+        {
+            currentHeartIndex = 1;
+        }
+        if (healthReduced >= 5)
+        {
+            currentHeartIndex = 2;
+        }
+    }
+
     public void ReduceHealth()
     {
-        
+        healthReduced++;
+        CheckCurrentHeart();
+        ChangeHeartSprite();
+
+    }
+
+    private void ChangeHeartSprite()
+    {
+        Sprite currentHeart = healthTracker[currentHeartIndex].GetComponent<UnityEngine.UI.Image>().sprite;
+        if (currentHeart == availableHealth[0])
+        {
+            healthTracker[currentHeartIndex].GetComponent<UnityEngine.UI.Image>().sprite = availableHealth[1];
+        }
+        else if (currentHeart == availableHealth[1])
+        {
+            healthTracker[currentHeartIndex].GetComponent<UnityEngine.UI.Image>().sprite = availableHealth[2];
+
+        }
     }
 }
