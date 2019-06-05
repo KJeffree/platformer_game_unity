7ea297deff3c5c4793be41a401f96559e977eadb
KJeffree <k.jeffree@gmail.com>
2019-06-05 10:42:42 +0100

    added code to saw to make it rotate

---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.rotation = -45f;
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.rotation -= 4.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
